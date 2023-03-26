9ee0b4888e874d2a21631901597a3ca11bbd83a2
peter <peterbrady17@outlook.com>
2023-03-26 06:45:55 -0400

    add runner script to handle build and dropping into interactive development shell, add pip3 requirements file, add better dockerfile and docker-compose support for host user ownership of file inside the docker container

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Repo designed to pull/run specific, already built, docker images from a specific docker repo
 
 `NOTE` this repo is designed for developers to run a dev docker image, and therefore mounts the users home directory into the image.
+`NOTE` this repo assumes the developer is using bash
 
 ### Info on running an image
 
@@ -17,10 +18,19 @@ optional arguments:
   -o OUTPUT_DIR, --output-dir OUTPUT_DIR
                         desired location for the generated Dockerfile and docker-compose file. Default is this current directory
   -u USER, --user USER  name of the user to be created in the image
+  -n IMAGE_NAME --image-name IMAGE_NAME "name of the local image to be created. Default is 'local-img-rnr'
   -l {info,warning,debug,error}, --log-level {info,warning,debug,error}
                         desired level of logging
 ```
 
 ### Example
-`./configure.py -i ubuntu-dev -o test`
-This generates a Dockerfile and docker-compose file in a directory called `test`.
+```
+mkdir dev
+
+./configure.py -i ubuntu-dev -o dev
+#This generates a Dockerfile and docker-compose file in a directory called `dev`.
+
+dev/runner.sh
+# This step build and runs the newly configured image and drops you in an interactive shell
+NOTE: your home directory will be mounted in the container at this step.
+```
--- configure.py
+++ configure.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 from logging import info, warning, debug, error
 import sys
+import os
 
 #logging dict
 log_values = {'info': logging.INFO, 'warning': logging.WARN, 'debug': logging.DEBUG, 'error': logging.ERROR}
@@ -17,6 +18,7 @@ def get_args():
     parser.add_argument('-r', '--registry', help="Desired base image registry to pull from. Default is my github registry", default="ghcr.io/prb17")
     parser.add_argument('-o', "--output-dir", help="desired location for the generated Dockerfile and docker-compose file. Default is this current directory", default="./")
     parser.add_argument('-u', "--user", help="name of the user to be created in the image", default="img-rnr")
+    parser.add_argument('-n', "--image-name", help="name of the local image to be created", default="local-img-rnr")
     parser.add_argument('-l', "--log-level", choices=list(log_values.keys()), help="desired level of logging", default="info")
     args = parser.parse_args()
     return args
@@ -32,7 +34,7 @@ def main():
 
     logging.info("setting up jinja environment")
     env = Environment (
-        loader=PackageLoader("__main__"),
+        loader=PackageLoader("configure"),
         autoescape=select_autoescape()
     )
     
@@ -41,30 +43,48 @@ def main():
     logging.info(f'pulling image: \'{pull_image}\'')
     dockerfile_template = "Dockerfile.jinja"
     composefile_template = "docker-compose.jinja"
+    runner_template = "runner.jinja"
     logging.debug(f'using Dockerfile template: \'{dockerfile_template}\'')
     logging.debug(f"using docker-compose template: '{composefile_template}'")
+    logging.debug(f"using run template: '{runner_template}'")
+
     # looks automatically in './templates' directory
     docker_template = env.get_template(dockerfile_template)
     compose_template = env.get_template(composefile_template)
+    runner_template = env.get_template(runner_template)
 
     
-    logging.info("rendering the Dockerfile and docker-compose.yml content based on the configuration values")
-    dockerfile_contents = docker_template.render(pull_image=pull_image, user_name=prog_args.user)
-    compose_contents = compose_template.render(pull_image=pull_image, user_name=prog_args.user)
+    logging.info("rendering the content based on the configuration values")
+    dockerfile_contents = docker_template.render(pull_image=pull_image)
+    compose_contents = compose_template.render(pull_image=pull_image, 
+                                                base_image=prog_args.base_image,
+                                                user_name=prog_args.user,
+                                                local_image_name=prog_args.image_name,
+                                                docker_context=prog_args.output_dir,
+                                                user_id=os.getuid(),
+                                                group_id=os.getgid())
+    runner_contents = runner_template.render(docker_context=prog_args.output_dir)
     logging.debug(f'What will be written to the Dockerfile:\n\'{dockerfile_contents}\'')
     logging.debug(f"What will be written to the docker-compose.yml file:\n'{compose_contents}'")
+    logging.debug(f"What will be written to the runner.sh file:\n'{runner_contents}'")
 
 
     logging.info("saving configured templates to Dockerfile and docker-compose.yml")
-    dockerfile_output = prog_args.output_dir + '/Dockerfile'
+    dockerfile_output = prog_args.output_dir + './Dockerfile'
     with open(dockerfile_output, 'w') as file:
         file.write(dockerfile_contents + "\n")
     logging.debug(f'Dockerfile saved to: \'{dockerfile_output}\'')
 
-    compose_output = prog_args.output_dir + '/docker-compose.yml'
+    compose_output = prog_args.output_dir + './docker-compose.yml'
     with open(compose_output, 'w') as file:
         file.write(compose_contents + "\n")
     logging.debug(f'docker-compose.yml saved to: \'{compose_output}\'')
+
+    runner_output = prog_args.output_dir + './runner.sh'
+    with open(runner_output, 'w') as file:
+        file.write(runner_contents + "\n")
+        os.chmod(runner_output, 0o500)
+    logging.debug(f'runner.sh saved to: \'{runner_output}\'')
 
     logging.info("Done")
 
